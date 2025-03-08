53a8cdd03c8520ba25df05d01d4da20d9d5adb15
M1D0R1x <veerababusaviti21@gmail.com>
2025-03-09 04:01:06 +0530

    deploy debug v12

--- build_files.sh
+++ build_files.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-
-# Use the Vercel-provided Python binary
+# Ensure Python 3.x is used
+python3.9 -m pip install --upgrade pip
 python3.9 -m pip install -r requirements.txt
-
-# Run migrations
-python3.9 manage.py migrate
-
-# Collect static files
-python3.9 manage.py collectstatic --noinput
-
-# Create a build directory for Vercel
-mkdir -p staticfiles_build
-cp -r staticfiles/* staticfiles_build/
