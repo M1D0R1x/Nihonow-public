7194b4f7e164eddcaf607691cf1b4e5ba6ddff17
M1D0R1x <veerababusaviti21@gmail.com>
2025-03-09 04:03:19 +0530

    deploy debug v13

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Ensure Python 3.x is used
-python3.9 -m pip install --upgrade pip
-python3.9 -m pip install -r requirements.txt
+pip install -r requirements.txt
+
+python3.9 manage.py collectstatic
