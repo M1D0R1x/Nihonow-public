85a2956ab88a86f07c8171fa1d908333935a0fa5
M1D0R1x <veerababusaviti21@gmail.com>
2025-03-09 03:57:44 +0530

    deploy debug v10

--- build_files.sh
+++ build_files.sh
@@ -8,7 +8,3 @@ python manage.py migrate
 
 # Collect static files
 python manage.py collectstatic --noinput
-
-# Create a build directory for Vercel
-mkdir -p staticfiles_build
-cp -r staticfiles/* staticfiles_build/
