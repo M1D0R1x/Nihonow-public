d03638e6da7be895dde5ca7fcaef492ac6b71a44
M1D0R1x <veerababusaviti21@gmail.com>
2025-03-09 20:22:43 +0530

    changed mail delivery

--- NihongoDekita/settings.py
+++ NihongoDekita/settings.py
@@ -150,8 +150,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'veerababusaviti2103@gmail.com'
-EMAIL_HOST_PASSWORD = 'ylgz mnht lugn kkuc'
+EMAIL_HOST_USER = 'veerababusaviti21@gmail.com'
+EMAIL_HOST_PASSWORD = 'ilkg mkaz mela bbuy'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 AUTHENTICATION_BACKENDS = [
