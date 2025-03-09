d79c3c7641c031466b8422442c4f8d915c403dce
M1D0R1x <veerababusaviti21@gmail.com>
2025-03-09 22:12:23 +0530

    added support for sitemap for SEO debug2

--- treasures/sitemaps.py
+++ treasures/sitemaps.py
@@ -5,7 +5,14 @@ from django.urls import reverse
 class StaticViewSitemap(Sitemap):
     changefreq = "daily"
     priority = 0.9
+
     def items(self):
-        return ['home']  # Just one URL
+        return [
+            'home', 'about', 'resources', 'contact', 'numbers',
+            'hiragana', 'katakana', 'n5', 'n4', 'n3', 'n2', 'n1',
+            'n5_quiz', 'n4_quiz', 'n3_quiz', 'n2_quiz', 'n1_quiz',
+            'n5_kanji', 'n4_kanji', 'n3_kanji', 'n2_kanji', 'n1_kanji'
+        ]
+
     def location(self, item):
         return reverse(item)
