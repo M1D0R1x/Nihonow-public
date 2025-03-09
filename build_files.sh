449b44bad802e7f05d1718f38832e9ea95cf1071
M1D0R1x <veerababusaviti21@gmail.com>
2025-03-09 11:49:17 +0530

    deploy debug v16

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "BUILD START"
+python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
 python3 NihongoDekita/manage.py collectstatic --noinput --clear
 mv staticfiles public/static
