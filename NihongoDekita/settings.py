6717a0c41c5f37de353b7925a82cebec82f5cafc
M1D0R1x <veerababusaviti21@gmail.com>
2025-03-10 10:53:04 +0530

    general maintenance

--- NihongoDekita/settings.py
+++ NihongoDekita/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-po^n&a(ii85f@0xqb_q$*pq#3m8^qcup-pkmof7b*(9bs$%m27
 
 #
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', '.now.sh']
 
