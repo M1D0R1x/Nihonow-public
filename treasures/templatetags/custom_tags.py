2cac8ba6a99bd709b349149db9dec8a56e7b23ee
M1D0R1x <veerababusaviti21@gmail.com>
2025-03-12 23:09:32 +0530

    updated custom tags logic

--- treasures/templatetags/custom_tags.py
+++ treasures/templatetags/custom_tags.py
@@ -1,11 +1,18 @@
 from django import template
 from django.utils.timezone import now
+import pytz
 
 register = template.Library()
 
+
 @register.simple_tag
 def get_greeting(username):
-    current_hour = now().hour
+    # Explicitly use the TIME_ZONE from settings
+    from django.conf import settings
+    tz = pytz.timezone(settings.TIME_ZONE)  # Should be 'Asia/Kolkata'
+    current_time = now().astimezone(tz)
+    current_hour = current_time.hour
+
     if current_hour < 12:
         return f"Ohayo Gozaimasu {username}!"
     elif current_hour < 18:
